d6bbbae2601c1feec092bd4749e4a601cf92108a
zefty <jaime.wu011@gmail.com>
2021-07-31 14:51:26 +1200

    add server main file python

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask, json, request
+import requests
+
+api = Flask(__name__)
+
+@api.route('/bert', methods = ['POST', 'GET'])
+def bert():
+  uri = "http://d449ac5c-5942-4da0-a42b-41e46814b152.australiaeast.azurecontainer.io/score"
+  headers = {"Content-Type": "application/json"}
+  if request.method == 'GET':
+    data = {
+        "data": "My hope lay in Jack's promise that he would keep a bright light burning in the upper story to guide me on my course.",
+    }
+  if request.method == 'POST':
+    data = json.loads(request.data)
+  data = json.dumps(data)
+  response = requests.post(uri, data = data, headers = headers)
+  return json.dumps(response.json())
+
+if __name__ == '__main__':
+    api.run() 
