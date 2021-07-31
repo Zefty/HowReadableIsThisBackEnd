9457b81d2d9c8ac43cc225bde733d63772a9b9ee
zefty <jaime.wu011@gmail.com>
2021-07-31 15:40:50 +1200

    json

--- app.py
+++ app.py
@@ -14,8 +14,8 @@ def bert():
         "data": "My hope lay in Jack's promise that he would keep a bright light burning in the upper story to guide me on my course.",
     }
   if request.method == 'POST':
-    print(request.data)
-    data = json.loads(request.data)
+    print(request.json)
+    data = json.loads(request.json)
   data = json.dumps(data)
   response = requests.post(uri, data = data, headers = headers)
   return json.dumps(response.json())
